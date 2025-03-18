ff02af21985107856f3094373b6293729b42b613
RobertJohnSoler <rsoler@hawk.iit.edu>
2025-03-17 21:04:57 -0500

    finished lvl1 buffer overflow

--- level1/vuln.c
+++ level1/vuln.c
@@ -3,15 +3,20 @@
 
 
 void vuln_func(){
-    char buffer[40];
+    char buffer[20];
+    int secret = 15;
     printf("Enter an input.\n");
     gets(buffer);
+    // If your input is longer than 20 chars, then your secret variable will be overwritten and will no longer be 15.
     printf("Your input is: %s \n", buffer);
+    printf("Secret is: %i \n", secret);
 }
 
 int main(){
     printf("Buffer overflow level 1...\n");
     vuln_func();
-    printf("Buffer overflow failed. Exiting normally...\n");
+
+    // If you overflow the buffer in vuln_func(), then these lines shouldn't be executed.
+    printf("Buffer overflow failed. Exiting normally...\n"); 
     return 0;
 }
