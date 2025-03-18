37d28934a69db5ebb42f7acb04ad27b0fa7907e0
RobertJohnSoler <rsoler@hawk.iit.edu>
2025-03-17 20:56:07 -0500

    removed win() func

--- level1/vuln.c
+++ level1/vuln.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-void win(){
-    printf("Buffer overflow successful! \n");
-}
 
 void vuln_func(){
     char buffer[40];
     printf("Enter an input.\n");
     gets(buffer);
-    printf("You went past the buffer!\n");
+    printf("Your input is: %s \n", buffer);
 }
 
 int main(){
